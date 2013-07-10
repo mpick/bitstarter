0e3d74118d8fe651b8b9479ed0c8f8691d423008
Miles Pickens <milespickens@gmail.com>
2013-07-10 08:45:21 +0000

    Trying to read from a file

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
-var express = require('express');
-
+var express = require('express'),
+	fs = require('fs');
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var content = fs.readFileSync(__dirname + 'index.html');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+  console.log(content);
 });
